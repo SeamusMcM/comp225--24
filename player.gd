45a150e2e16dc2e5c4313d72a56b0d2917d56d4f
SeamusMcM <smcmurre@macalester.edu>
2024-09-30 20:47:35 -0500

    Further progress on the tutorial

--- player.gd
+++ player.gd
@@ -7,7 +7,7 @@ var screen_size
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	screen_size=get_viewport_rect().size
-	hide()
+	#hide()
 
 func _process(delta):
 	var velocity = Vector2.ZERO
@@ -29,11 +29,20 @@ func _process(delta):
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size) #clamp prevents leaving screen
 	
+	$AnimatedSprite2D.flip_v = velocity.y > 0
 	if velocity.x != 0:
 		$AnimatedSprite2D.animation = "walk"
-		$AnimatedSprite2D.flip_v = false
 		$AnimatedSprite2D.flip_h = velocity.x < 0
 	elif velocity.y != 0:
 		$AnimatedSprite2D.animation = "up"
-		$AnimatedSprite2D.flip_v = velocity.y > 0
 		
+
+func _on_body_entered(body: Node2D) -> void:
+	hide()
+	hit.emit()
+	$CollisionShape2D.set_deferred("disabled", true)
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
