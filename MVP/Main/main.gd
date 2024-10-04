77e71751a58e14694476055be92c19400132964e
firegittruck <124188941+firegittruck@users.noreply.github.com>
2024-10-04 15:27:05 -0500

    Created Main Scene

---
+++ MVP/Main/main.gd
@@ -0,0 +1,59 @@
+extends Node
+
+@export var obstacle_scene: PackedScene
+var time
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	new_game()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func p1_game_over() -> void:
+	$TimeTimer.stop()
+	$ObstacleTimer.stop()
+
+
+func p2_game_over() -> void:
+	$TimeTimer.stop()
+	$ObstacleTimer.stop()
+
+func new_game():
+	time = 0
+	$Player1.start($StartPosition1.position)
+	$Player2.start($StartPosition2.position)
+	$StartTimer.start()
+
+
+func _on_obstacle_timer_timeout() -> void:
+	# Create a new instance of the Mob scene.
+	var obstacle = obstacle_scene.instantiate()
+
+	# Choose a random location on Path2D.
+	var obstacle_spawn_location = $ObstaclePath/ObstacleSpawnLocation
+	obstacle_spawn_location.progress_ratio = randf()
+
+	# Set the mob's direction perpendicular to the path direction.
+	var direction = obstacle_spawn_location.rotation + PI / 2
+
+	# Set the mob's position to a random location.
+	obstacle.position = obstacle_spawn_location.position
+
+	# Choose the velocity for the mob.
+	var velocity = Vector2(150.0, 0.0)
+	obstacle.linear_velocity = velocity.rotated(direction)
+
+	# Spawn the mob by adding it to the Main scene.
+	add_child(obstacle)
+
+
+func _on_time_timer_timeout() -> void:
+	time += 1
+
+func _on_start_timer_timeout() -> void:
+	$ObstacleTimer.start()
+	$TimeTimer.start()
--- MVP/Player1/p1scrpt.gd
+++ MVP/Player1/p1scrpt.gd
@@ -14,13 +14,13 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var velocity = Vector2.ZERO # The player's movement vector.
-	if Input.is_action_pressed("p2_right"):
+	if Input.is_action_pressed("p1_right"):
 		velocity.x += 1
-	if Input.is_action_pressed("p2_left"):
+	if Input.is_action_pressed("p1_left"):
 		velocity.x -= 1
-	if Input.is_action_pressed("p2_down"):
+	if Input.is_action_pressed("p1_down"):
 		velocity.y += 1
-	if Input.is_action_pressed("p2_up"):
+	if Input.is_action_pressed("p1_up"):
 		velocity.y -= 1
 
 	if velocity.length() > 0:
