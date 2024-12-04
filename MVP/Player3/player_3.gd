bd72c8089a8ebfcd3e27674eb2fae801370e8c68
armandoA-N <124317519+armandoA-N@users.noreply.github.com>
2024-12-04 14:27:32 -0600

    fixed some issues that showed up from vs code

--- MVP/Player3/player_3.gd
+++ MVP/Player3/player_3.gd
@@ -64,24 +64,24 @@ func _on_body_entered(body: Node2D) -> void:
 		GlobalScript._p1_points_earned(int(100))
 	elif body.get_nombre() == "shield":
 		body.queue_free()
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
+#<<<<<<< HEAD
+#<<<<<<< HEAD
+#<<<<<<< HEAD
 		animation = "shield"
 		set_collision_mask_value(1,false)
 		set_collision_mask_value(2,true)
 		$AnimatedSprite2D.animation = animation
 		$ShieldTimer.start()
 		AudioController.play_shield()
-=======
+#=======
 		GlobalScript.set_p1_item("shield")
->>>>>>> 146bce864289e3bf3605b7990889fb091a190968
-=======
+#>>>>>>> 146bce864289e3bf3605b7990889fb091a190968
+#=======
 		GlobalScript.set_p1_item("shield")
->>>>>>> 146bce864289e3bf3605b7990889fb091a190968
-=======
+#>>>>>>> 146bce864289e3bf3605b7990889fb091a190968
+#=======
 		GlobalScript.set_p1_item("shield")
->>>>>>> 146bce864289e3bf3605b7990889fb091a190968
+#>>>>>>> 146bce864289e3bf3605b7990889fb091a190968
 	else:
 		hide() # Player disappears after being hit.
 		#hit.emit()
@@ -117,16 +117,16 @@ func _on_losing_shield_timer_timeout() -> void:
 	set_collision_mask_value(1,true)
 	set_collision_mask_value(2,false)
 	$LosingShieldTimer.stop()
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
+#<<<<<<< HEAD
+#<<<<<<< HEAD
+#<<<<<<< HEAD
 	AudioController.stop_shield()
-=======
+#=======
 	GlobalScript.set_p1_item("none")
->>>>>>> 146bce864289e3bf3605b7990889fb091a190968
-=======
+#>>>>>>> 146bce864289e3bf3605b7990889fb091a190968
+#=======
 	GlobalScript.set_p1_item("none")
->>>>>>> 146bce864289e3bf3605b7990889fb091a190968
-=======
+#>>>>>>> 146bce864289e3bf3605b7990889fb091a190968
+#=======
 	GlobalScript.set_p1_item("none")
->>>>>>> 146bce864289e3bf3605b7990889fb091a190968
+#>>>>>>> 146bce864289e3bf3605b7990889fb091a190968
